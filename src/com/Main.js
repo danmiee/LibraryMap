2ff53b8c0101de4edcb32159d147792ebdf0b1f7
dan-mi <dan-mi@kakao.com>
2022-11-28 00:13:18 +0900

    MapContainer 분리 및 아이콘 버튼화
      - 현위치 지도보기 아이콘, 보기 선택 토글 아이콘 코드 div에서 button으로 변경
      - 목록으로 보기 코드 div에서 버튼으로 변경
      - MapContainer.js 생성하여 kakaomap 불러오기 적용하였으나 해당 컴포넌트를 넣으면 다른 요소 모두 사라지고 지도도 안나오므로 추가 확인 필요

--- src/com/Main.js
+++ src/com/Main.js
@@ -1,34 +1,37 @@
 import '../css/Main.css';
+import MapContainer from './MapContainer'
 
 function Main() {
-  return (
+	return (
 		<>
-			{/* 검색창 만들기
-				제출하면 Search 화면으로 이동 */}
-			<form className='searchText'>
-				<input type='text' />
-				<input type='submit' />
-			</form>
-			{/* 현재위치 기준 지도 불러오기
-			초기화면 : 현재위치, 전체보기
-			드래그로 이동 및 확대, 축소 기능 필요 */}
-			<div className='bg'>
-				{/* 도서관 위치 및 운영중 여부 표시 */}
-				{/* 표식 클릭하면 해당 도서관 정보로 Info 화면 보여주기 */}
-			</div>
-			{/* 현위치 지도보기 */}
-			<div className='crntLocation'>
-			</div>
-			{/* 토글로 전체보기 or 운영중인 곳만 보기 선택 */}
-			<div className='viewToggle'>
-			</div>
-			{/* 목록으로 보기 버튼
-				클릭하면 List 화면 보여주기 */}
-			<div className='viewList'>
+				{/* 검색창 만들기 */}
+				{/* 기능 : 제출하면 Search 화면으로 이동 */}
+				<form className='searchText'>
+					<input type='text' />
+					<input type='submit' />
+				</form>
 
+				{/* 현위치 지도보기 아이콘 */}
+				<button className='crntLocation' aria-label='현재위치 표시 버튼'>
+					현위치
+				</button>
+
+				{/* 전체보기 or 운영중인 곳만 보기 선택 토글 */}
+				<button className='viewToggle' aria-label='보기 형식 선택 버튼'>
+					보기 선택
+				</button>
+
+				{/* 목록으로 보기 버튼 */}
+				{/* 기능 : 클릭하면 List 화면 보여주기 */}
+				<button className='viewList' aria-label='목록으로 보기 버튼'>
+					목록으로 보기
+				</button>
+			{/* 지도 가져오기 */}
+			<div>
+				{/* <MapContainer /> */}
 			</div>
-    </>
-  );
+		</>
+	);
 }
 
 export default Main;
