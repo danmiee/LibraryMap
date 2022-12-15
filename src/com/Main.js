ba0a6c7f1d59f6469ca7f901a9cccf29ef5bf9e8
danmiee <dan-mi@kakao.com>
2022-12-15 21:55:43 +0900

    main 컴포넌트 수정
      - <>를 <div className='main'>로 변경
      - List 관련 태그 변경
      - css 적용

--- src/com/Main.js
+++ src/com/Main.js
@@ -3,34 +3,36 @@ import MapContainer from './MapContainer'
 
 function Main() {
 	return (
-		<>
-				{/* 검색창 만들기 */}
-				{/* 기능 : 제출하면 Search 화면으로 이동 */}
-				<form className='searchText'>
-					<input type='text' />
-					<input type='submit' />
-				</form>
+		<div className='main'>
+			{/* 검색창 만들기 */}
+			{/* 기능 : 제출하면 Search 화면으로 이동 */}
+			<form className='searchText'>
+				<input type='text' />
+				<input type='submit' />
+			</form>
 
-				{/* 현위치 지도보기 아이콘 */}
-				<button className='crntLocation' aria-label='현재위치 표시 버튼'>
-					현위치
-				</button>
+			{/* 현위치 지도보기 아이콘 */}
+			<button className='crntLocation' aria-label='현재위치 표시 버튼'>
+				현위치
+			</button>
 
-				{/* 전체보기 or 운영중인 곳만 보기 선택 토글 */}
-				<button className='viewToggle' aria-label='보기 형식 선택 버튼'>
-					보기 선택
-				</button>
+			{/* 전체보기 or 운영중인 곳만 보기 선택 토글 */}
+			<button className='viewToggle' aria-label='보기 형식 선택 버튼'>
+				보기 선택
+			</button>
 
-				{/* 목록으로 보기 버튼 */}
-				{/* 기능 : 클릭하면 List 화면 보여주기 */}
-				<button className='viewList' aria-label='목록으로 보기 버튼'>
+			{/* 목록으로 보기 버튼 */}
+			{/* 기능 : 클릭하면 List 화면 보여주기 */}
+			<div className='viewList'>
+				<button className='viewButton' aria-label='목록으로 보기 버튼'>
 					목록으로 보기
 				</button>
+			</div>
 			{/* 지도 가져오기 */}
 			<div>
 				{/* <MapContainer /> */}
 			</div>
-		</>
+		</div>
 	);
 }
 
